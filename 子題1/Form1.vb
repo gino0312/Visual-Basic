5b4594f732eb15a5c8581f493b7365fe4505ab5c
gino0312 <gino.hsu2002@gmail.com>
2018-10-21 14:58:44 +0800

    子題1

---
+++ 子題1/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        FileOpen(1, "in1.txt", OpenMode.Input)
+        FileOpen(2, "out.txt", OpenMode.Output)
+        Dim str1 As String = LineInput(1)
+        Do Until EOF(1)
+            Dim str() As String = LineInput(1).Split(" ")
+            Dim n As Integer = Str.Length
+            Dim count As Integer = 0
+            For i = 0 To n - 1
+                count = count + str(i).Length
+            Next
+            PrintLine(2, count)
+        Loop
+        FileClose()
+        Close()
+    End Sub
+End Class
